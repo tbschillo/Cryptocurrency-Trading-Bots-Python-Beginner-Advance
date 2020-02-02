9718f59f6e7e36e10a1312530785b6e10ef4be89
tbschillo <56337458+tbschillo@users.noreply.github.com>
2020-02-02 16:01:38 -0500

    Update BinanceKeys.py

--- BinanceKeys.py
+++ BinanceKeys.py
@@ -1,3 +1,2 @@
 BinanceKey1 = {'api_key': '44zOwQJtuHN1cEBcq55BEtTyGCYhGvF181u2b00RpzzRIp9EttItaGcJVAIx30cZ',
     'api_secret':'OyED2tyZbj4O1PwvJQejVQ1cDxQoVxODY3Z5u4CcsVRABZwBwCQGzoym8mPwZtQm'}
-	
